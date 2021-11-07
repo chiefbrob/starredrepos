db902d4379f04e3963637005c8fbf99a2e4c9bc7
Brian Chief Obare <brian.obare@pfizer.com>
2021-11-07 17:03:42 +0300

    missing files

--- resources/js/components/style/Style.vue
+++ resources/js/components/style/Style.vue
@@ -3,10 +3,10 @@
     <nav-root></nav-root>
     <file-uploader class="mb-5 pb-5"></file-uploader>
     <div class="row">
-        <div class="col-md-4">
-            <phone-number @updated="phoneUpdated"></phone-number>
-        </div>
-        <div class="col-md-4">Phone: {{ phone_number }}</div>
+      <div class="col-md-4">
+        <phone-number @updated="phoneUpdated"></phone-number>
+      </div>
+      <div class="col-md-4">Phone: {{ phone_number }}</div>
     </div>
     <page-footer></page-footer>
   </div>
@@ -14,15 +14,15 @@
 
 <script>
   export default {
-      data() {
-          return {
-              phone_number: null,
-          }
+    data() {
+      return {
+        phone_number: null,
+      };
+    },
+    methods: {
+      phoneUpdated(phone_number) {
+        this.phone_number = phone_number;
       },
-      methods: {
-          phoneUpdated(phone_number) {
-              this.phone_number = phone_number;
-          }
-      }
+    },
   };
 </script>
