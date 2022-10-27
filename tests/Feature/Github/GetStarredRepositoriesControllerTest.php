5fc27d4258826fa2df6766e0be80355ca3eb30f0
Brian Chief Obare <brian.obare@pfizer.com>
2022-10-27 12:44:39 +0300

    failing test

--- tests/Feature/Github/GetStarredRepositoriesControllerTest.php
+++ tests/Feature/Github/GetStarredRepositoriesControllerTest.php
@@ -4,12 +4,13 @@ namespace Tests\Feature\Github;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+//use Tests\Feature\MocksGithubAPI;
 use Tests\TestCase;
-use Tests\Feature\MocksGithubAPI;
 
 class GetStarredRepositoriesControllerTest extends TestCase
 {
-    use RefreshDatabase, MocksGithubAPI;
+    use RefreshDatabase;
+    //use MocksGithubAPI;
     /**
      * A basic feature test example.
      *
