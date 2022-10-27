9cdea38594e91c1506173d66b3137b8099401159
Brian Chief Obare <brian.obare@pfizer.com>
2022-10-27 12:29:58 +0300

    users can delete token

--- tests/Feature/Github/GetStarredRepositoriesControllerTest.php
+++ tests/Feature/Github/GetStarredRepositoriesControllerTest.php
@@ -6,6 +6,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 //use Tests\Feature\MocksGithubAPI;
 use Tests\TestCase;
+use Tests\Feature\MocksGithubAPI;
 
 class GetStarredRepositoriesControllerTest extends TestCase
 {
