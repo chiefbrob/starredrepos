770957427659cb38698d263081391e489dc96ed9
Brian Chief Obare <brianobare@gmail.com>
2023-04-16 10:45:52 +0300

    slim down contact form

--- resources/js/components/pages/ContactRoot.vue
+++ resources/js/components/pages/ContactRoot.vue
@@ -2,90 +2,34 @@
   <div>
     <div class="mb-5 pb-5 row" v-if="!submitted">
       <div class="col-md-8 offset-md-2 py-4">
-        <b-progress :max="max" show-progress animated>
-          <b-progress-bar
-            :value="value"
-            :label="`${((value / max) * 100).toFixed(0)}%`"
-          ></b-progress-bar>
-        </b-progress>
         <h4>Contact</h4>
-        <b-form-group
-          class="pt-3"
-          id="input-group-1"
-          label="Name: *"
-          label-for="name"
-          description="Full Name"
-        >
-          <b-form-input
-            id="name"
-            v-model="form.name"
-            type="text"
-            required
-            :placeholder="`Enter all three names`"
-          ></b-form-input>
+        <b-form-group class="pt-3" id="input-group-1" label="Name: *" label-for="name">
+          <b-form-input id="name" v-model="form.name" type="text" required></b-form-input>
         </b-form-group>
         <field-error :solid="false" :errors="errors" field="name"></field-error>
 
-        <b-form-group
-          id="input-group-2"
-          label="Email: *"
-          label-for="email"
-          description="Email address"
-        >
-          <b-form-input
-            id="email"
-            v-model="form.email"
-            type="email"
-            required
-            :placeholder="`someone@somewhere.something`"
-          ></b-form-input>
+        <b-form-group id="input-group-2" label="Email: *" label-for="email">
+          <b-form-input id="email" v-model="form.email" type="email" required></b-form-input>
         </b-form-group>
         <field-error :solid="false" :errors="errors" field="email"></field-error>
 
-        <b-form-group
-          id="input-group-4"
-          label="Phone Number: *"
-          label-for="phone_number"
-          description="Complete phone number with country code e.g. 254722222222"
-        >
+        <b-form-group id="input-group-4" label="Phone Number: *" label-for="phone_number">
           <b-form-input
             id="phone_number"
             v-model="form.phone_number"
             type="text"
             required
-            placeholder="254722222222"
           ></b-form-input>
         </b-form-group>
         <field-error :solid="false" :errors="errors" field="phone_number"></field-error>
 
-        <b-form-group
-          id="input-group-5"
-          label="Title: *"
-          label-for="title"
-          description="Subject message"
-        >
-          <b-form-input
-            id="title"
-            v-model="form.title"
-            type="text"
-            required
-            :placeholder="`Briefly`"
-          ></b-form-input>
+        <b-form-group id="input-group-5" label="Title: *" label-for="title">
+          <b-form-input id="title" v-model="form.title" type="text" required></b-form-input>
         </b-form-group>
         <field-error :solid="false" :errors="errors" field="title"></field-error>
 
-        <b-form-group
-          id="input-group-3"
-          label="Message: *"
-          label-for="message"
-          description="Write something complete"
-        >
-          <b-form-textarea
-            id="message"
-            v-model="form.contents"
-            required
-            placeholder="..."
-          ></b-form-textarea>
+        <b-form-group id="input-group-3" label="Message: *" label-for="message">
+          <b-form-textarea id="message" v-model="form.contents" required></b-form-textarea>
         </b-form-group>
         <field-error :solid="false" :errors="errors" field="contents"></field-error>
         <p class="py-2">
